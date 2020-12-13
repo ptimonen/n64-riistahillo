d5072bb034bd74556bb8d16fa70cac55c9241097
pt <pt@debian.debian.local>
2020-12-13 12:26:32 +0200

    Fix cen64

--- scripts/run_cen64.sh
+++ scripts/run_cen64.sh
@@ -4,4 +4,4 @@ set -eu
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd ${DIR}/../tools/cen64
-./cen64-debian9-x86_64 pifdata.bin ${DIR}/../riistahillo.n64
+./cen64-debian9-x86_64 pifdata.bin ${DIR}/../kumi-daiko-beatoff-64.n64
